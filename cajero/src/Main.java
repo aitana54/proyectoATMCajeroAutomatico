14bfb6843e0af8244aaaef50f36d3491a17793f2
aitana54 <aitana.buiza@gracia.lasalle.cat>
2024-12-31 11:54:11 +0100

    Creación de variable global

--- cajero/src/Main.java
+++ cajero/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
+    Scanner sc = new Scanner(System.in); //Variable global
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }

f0a55e42b7defcc8598ccfa6cea726d87cea3386
aitana54 <aitana.buiza@gracia.lasalle.cat>
2024-12-31 12:26:21 +0100

    Comentar método

--- cajero/src/Main.java
+++ cajero/src/Main.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class Main {
     Scanner sc = new Scanner(System.in); //Variable global
 
+    /**
+     * Muestra la bienvenida al programa
+     * @return String, bienvenida
+     */
     public static String bienvenida(){
         return "----------------------------------------\n" +
                 "       ATM CAJERO AUTOMÁTICO\n" +
                 "---------------------------------------";
     }
+
+
 
     public static void main(String[] args) {
         System.out.println(bienvenida());

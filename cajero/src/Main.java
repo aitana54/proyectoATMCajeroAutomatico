c1959b9c8421d3e6b7edf5421ecbfba846c50da1
aitana54 <aitana.buiza@gracia.lasalle.cat>
2024-12-31 12:23:29 +0100

    Creación método bienvenida y su salida por pantalla

--- cajero/src/Main.java
+++ cajero/src/Main.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     Scanner sc = new Scanner(System.in); //Variable global
-    
-    public static void main(String[] args) {
 
+    public static String bienvenida(){
+        return "----------------------------------------\n" +
+                "       ATM CAJERO AUTOMÁTICO\n" +
+                "---------------------------------------";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(bienvenida());
     }
 }
